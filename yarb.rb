b287235206ca2ebe62e37578b3c5f90b72012f31
Denis Laliberte <denis.laliberte@gmail.com>
2019-10-22 13:09:30 -0400

    put the default command in the configuration

--- yarb.rb
+++ yarb.rb
@@ -83,6 +83,7 @@ class Yarb
   end
 
   DEFAULT_CONFIG = {
+    'default_command' => 'eval',
     'alias' => {
       '-h' => '--help',
       '-d' => '--dry-run'
@@ -166,7 +167,7 @@ class Yarb
     elsif command?(args(0))
       execute_command(args(0))
     elsif args(0) && args(0).match(/\.yml$/)
-      @arguments.unshift('eval')
+      @arguments.unshift(@config['default_command'])
       execute
     else
       help
