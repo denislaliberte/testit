6cff8a6cbd2f2dc11e082bae45677e414e82e03d
Denis Laliberte <denis.laliberte@gmail.com>
2019-11-01 13:36:08 -0400

    Add command example

--- yarb.rb
+++ yarb.rb
@@ -1,10 +1,25 @@
+#!/usr/bin/env ruby
+
 class Yarb
   def initialize(arguments)
+    @data = Hash.new { |h, k| h[k] = {} }
+    @data['command']['name'] = arguments.first
   end
 
   def execute
+    return 'Installation instruction' if @data['command']['name'] == 'example'
     return <<~HELP
-      --help   output this message
+      Flags
+        --help:   output this message
+
+      Commands:
+        example:  command to return a example file
+
     HELP
   end
 end
+
+if caller.length == 0
+  puts Yarb.new(ARGV).execute
+end
+
--- yarb_test.rb
+++ yarb_test.rb
@@ -7,4 +7,8 @@ class YarbTest < Minitest::Test
   def test_help
     assert_match /--help.*output this message/, Yarb.new(['--help']).execute
   end
+
+  def test_example
+    assert_match /Installation/, Yarb.new(['example']).execute
+  end
 end
